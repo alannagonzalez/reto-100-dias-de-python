0ffc640db580b5fea8623b327c6c517a6084dc01
Alanna González <alannagonzalez2110@gmail.com>
2026-01-01 23:56:58 -0400

    Primer dia

---
+++ dia1.py
@@ -0,0 +1,8 @@
+"""Haz un programa que pida al usuario su nombre y su hobby, luego el programa debe saludarlo
+con dicha informacion y escribir un "Hola mundo"""
+
+nombre = input("Digite su nombre: ")
+pasatiempo = input("Digite su pasatiempo: ")
+
+print(f"Buenas {nombre}!, tu pasatiempo es {pasatiempo}")
+print("Hola mundo!")
